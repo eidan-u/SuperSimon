8a8fc7fb3b1164b76f2d8f9104da81747bf82216
Your Name <you@example.com>
2019-11-24 14:25:30 +0100

    supersimon2

---
+++ script.js
@@ -0,0 +1,16 @@
+let $start;
+let $restart;
+let $rouge = document.querySelector('.rouge');
+let $bleu = document.querySelector('.bleu');
+let $jaune = document.querySelector('.jaune');
+let $vert = document.querySelector('.vert');
+let $rules = document.querySelector('.regles');
+let $player = [];
+let $victoire = false;
+let $clique = document.querySelector('.clickList');
+let $do = new Audio('sound/do.wav');
+let $re = new Audio('sound/re.wav');
+let $mi = new Audio('sound/mi.wav');
+let $fa = new Audio('sound/fa.wav');
+
+
