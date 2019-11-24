6b1c8d5a9b5462c992f711f3352752073e759179
Your Name <you@example.com>
2019-11-24 15:28:56 +0100

    supersimon3

--- script.js
+++ script.js
@@ -13,4 +13,46 @@ let $re = new Audio('sound/re.wav');
 let $mi = new Audio('sound/mi.wav');
 let $fa = new Audio('sound/fa.wav');
 
+//on attribu un changement d'opacité à chaque couleur au click puis apres 2 seconde un retour à la normal/ et qui permet de jouer une note
+function blue() {
+    $bleu.style.opacity = "0.5";
+    setTimeout(function retour_couleur_bleu() {
+        $bleu.style.opacity = "1"
+    }, 500);
+    $do.play();
+}
+function yellow() {
+    $jaune.style.opacity = "0.5";
+    setTimeout(function retour_couleur_jaune() {
+        $jaune.style.opacity = "1"
+    }, 500);
+    $re.play();
+}
+function green() {
+    $vert.style.opacity = "0.5";
+    setTimeout(function retour_couleur_vert() {
+        $vert.style.opacity = "1"
+    }, 500);
+    $mi.play();
+}
+function red() {
+    $rouge.style.opacity = "0.5";
+    setTimeout(function retour_couleur_rouge() {
+        $rouge.style.opacity = "1"
+    }, 500);
+    $fa.play();
+}
+//afficher les regles
+function rulesOn() {
+    $rules.style.display = 'block';
+}
+//masquer les regles
+function rulesOff() {
+    $rules.style.display = 'none';
+}
+//la fonction Simon permet d'ajouter plus un à la clickList
+function Simon() {
+    $clique.innerHTML++;
+}
+
 
