799419c0513585abeb3dfa1c354c1ae4013c11b0
Your Name <you@example.com>
2019-11-24 16:22:01 +0100

    supersimon4

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function blue() {
     }, 500);
     $do.play();
 }
+
 function yellow() {
     $jaune.style.opacity = "0.5";
     setTimeout(function retour_couleur_jaune() {
@@ -28,6 +29,7 @@ function yellow() {
     }, 500);
     $re.play();
 }
+
 function green() {
     $vert.style.opacity = "0.5";
     setTimeout(function retour_couleur_vert() {
@@ -35,6 +37,7 @@ function green() {
     }, 500);
     $mi.play();
 }
+
 function red() {
     $rouge.style.opacity = "0.5";
     setTimeout(function retour_couleur_rouge() {
@@ -56,3 +59,77 @@ function Simon() {
 }
 
 
+//fonction qui permet de generer un nombre aléatoire
+
+function hasard() {
+    $nombre = Math.round(1 + Math.random() * 3);
+    return $nombre;
+}
+let $hasard = hasard();
+console.log($hasard);
+
+//une fonction qui permet d'afficher les valeur du Math.random à partir du bouton start et qui permet de donner une couleur pour chaque chiffre sous forme de condition
+
+function start() {
+
+    if ($hasard == 1) {
+        blue();
+    } else if ($hasard == 2) {
+        yellow();
+    } else if ($hasard == 3) {
+        green();
+    } else if ($hasard == 4) {
+        red();
+    } else {
+        console.log('erreur');
+    }
+
+    // on créé un tableau qui se rempli au fur et à mesure que la  fonction hasard() est jouéé, si le tableau possede 1 element, il stop la fonction hasard et retourne les valeur dans la console
+
+    let $compte = [];
+    let i = $compte.length;
+    while (i < 1) {
+        i++
+        $compte.push($hasard);
+    }
+    console.log($compte);
+
+    //le tour de la machine est terminé maintenant à celui du joueur
+
+    console.log('à vous de jouer!');
+
+    //on créé un evenement qui permet de rajouter une section au tableau du joueur
+
+    $bleu.addEventListener('click', function ChangeTab1() {
+        $player.push(1)
+        console.log($player)
+    });
+    $rouge.addEventListener('click', function ChangeTab2() {
+        $player.push(4)
+        console.log($player)
+    });
+    $jaune.addEventListener('click', function ChangeTab3() {
+        $player.push(2)
+        console.log($player)
+    });
+    $vert.addEventListener('click', function ChangeTab4() {
+        $player.push(3)
+        console.log($player)
+    });
+
+    if ($player == $compte) {
+        console.log('vous avez gagné');
+    } else {
+        console.log('vous avez perdu')
+    }
+}
+
+//appuyer sur le restart permet de reboot les deux tableaux ainsi que le nombre de cliques
+
+function restart() {
+    $compte = [];
+    $player = [];
+    console.log($compte);
+    console.log($player);
+    $clique.innerHTML = 0;
+}
